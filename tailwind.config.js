ed2e5de4e9cdb694b2300021822509b4bea5de0f
yash-damon <helloyash88@gmail.com>
2024-04-05 18:03:00 +0530

    first iteration of the project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./public/second.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100:"#e2e2d5",200:"#888883"
+        }
+      },
+      fontFamily: {
+        body:['Nunito']
+      }
+    },
+  },
+  plugins: [],
+};
